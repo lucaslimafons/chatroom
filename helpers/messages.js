e68cba3f0689a769b5be04ebffa169351331482d
Lucas Fonseca <lucaslimafons@gmail.com>
2020-08-23 16:10:40 -0300

    stooq get and parse data

--- helpers/messages.js
+++ helpers/messages.js
@@ -4,5 +4,8 @@ module.exports = {
   'password_required': 'Password is required.',
   'socket_required': 'Socket is required.',
   'username_used': 'Username already in use!',
-  'error_try_again': 'An error occurred, please try again later.'
+  'error_try_again': 'An error occurred, please try again later.',
+  'stock_code_required': 'Stock code is required.',
+  'stock_quote_message': '{CODE} quote is ${AMOUNT} per share.',
+  'stock_quote_error': 'Invalid stock quote please try another one.'
 }
